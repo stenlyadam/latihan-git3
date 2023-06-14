536675f808d8fbd04b7fbdf01499fa0097ad7959
rivaldo-bogar <androsbgr13@gmail.com>
2023-06-14 09:26:42 +0800

    adding line 5

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("Hello World2");
 console.log("hello saya bogar");
 console.log("ini line yg saya buat");
 console.log("line yang sama terjadi conflict");
+console.log("Automatic conflict");
