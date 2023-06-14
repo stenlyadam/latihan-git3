cbeb0c7c536560132a49d4bffd8a8151eb823c6d
rivaldo-bogar <androsbgr13@gmail.com>
2023-06-15 01:06:59 +0800

    Menambahakan clg

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("hello saya bogar");
 console.log("ini line yg saya buat");
 console.log("line yang sama terjadi conflict");
 console.log("Automatic conflict");
+console.log("ini bogar");
